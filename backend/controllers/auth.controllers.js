8c94e894bbd9337303e835244342c56894f7c856
mehar-khan0786 <khanmehar30032@gmail.com>
2026-05-02 06:48:45 +0530

    Update auth.controllers.js

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -34,8 +34,8 @@ export const signup=async(req,res)=>{
         res.cookie("token",token,{
             httpOnly:true,
             maxAge:7*24*60*60*1000,
-            sameSite:"lax",
-            secure:process.env.NODE_ENVIRONMENT==="production"
+            sameSite:"None",
+            secure:true
         });
 
        return res.status(201).json({user});
